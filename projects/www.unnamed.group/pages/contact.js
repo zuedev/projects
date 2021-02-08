59207a693ff6295608db22291110e04f254da7cc
zuedev <zuedev@gmail.com>
2021-02-08 21:07:02 +0000

    improve spam protection for contact page

--- projects/www.unnamed.group/pages/contact.js
+++ projects/www.unnamed.group/pages/contact.js
@@ -1,6 +1,18 @@
 export default function contact() {
   return (
-    <form name="contact" method="POST" action="/" netlify>
+    <form
+      name="contact"
+      method="POST"
+      data-netlify-recaptcha="true"
+      action="/index"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+    >
+      <p class="hidden">
+        <label>
+          Don’t fill this out if you’re human: <input name="bot-field" />
+        </label>
+      </p>
       <p>
         <label>
           Name: <input type="text" name="name" />
@@ -16,6 +28,7 @@ export default function contact() {
           Message: <textarea name="message"></textarea>
         </label>
       </p>
+      <div data-netlify-recaptcha="true"></div>
       <p>
         <button type="submit">Send</button>
       </p>
