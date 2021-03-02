1c40700b0c7be08e004db89ae7f6bc6ccda1401c
Alex <zuedev@gmail.com>
2021-03-02 19:31:27 +0000

    make token env var

--- active/unnamed-discord-bot/main.js
+++ active/unnamed-discord-bot/main.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js"),
 
 const client = new Discord.Client();
 
-client.login("token");
+client.login(process.env.TOKEN);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}! 🚀`);
